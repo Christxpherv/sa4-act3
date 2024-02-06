dce841d34ba85bdffc4792bdfa29f76489ebb3c6
Christopher Valle <christxpherv@gmail.com>
2024-02-06 09:36:24 -0600

    added unlimited tries and ability to quit program

--- guess_number.py
+++ guess_number.py
@@ -3,6 +3,12 @@ number = 10
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
+while guess != number:
+    guess = input("Nope! Try again or write \"q\" to exit: ")
+    if guess == "q":
+        break
+    guess = int(guess)
+
 if guess == number:
     print("Congratulations! You guessed the right number.")
 else:
