c2db9d98e6c5d07329df094a4eeac98b93384660
Christopher Valle <christxpherv@gmail.com>
2024-02-06 14:52:19 -0600

    added limitied tries, tells users their left attempts

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,20 @@
 number = 10
+remaining = 3
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-while guess != number:
+while remaining > 0 and guess != number:
+    remaining -= 1
+    print(f'remaining guesses: {remaining + 1}')
     guess = input("Nope! Try again or write \"q\" to exit: ")
-    if guess == "q":
-        break
-    guess = int(guess)
 
-if guess == number:
+    if (guess == "q") or (number == int(guess)):
+        break
+        
+if guess == "q":
+    print(f"You decided to quit! The number was {number}.")
+elif int(guess) == number:
     print("Congratulations! You guessed the right number.")
 else:
     print(f"Sorry! The number was {number}.")
