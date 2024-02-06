fc06125871e59a8b0f68739d77a5b2375576a9e7
Christopher Valle <christxpherv@gmail.com>
2024-02-06 09:49:13 -0600

    added hints to tell the user if their guess was high or low

--- guess_number.py
+++ guess_number.py
@@ -3,13 +3,16 @@ number = 10
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-while guess != number:
-    guess = input("Nope! Try again or write \"q\" to exit: ")
+while int(guess) != number:
     if guess == "q":
         break
-    guess = int(guess)
+    elif int(guess) > number:
+        print("Too high!", end = " ")
+    elif int(guess) < number:
+        print("Too low!", end = " ")
+    guess = input("Try again or write \"q\" to exit: ")
 
-if guess == number:
+if int(guess) == number:
     print("Congratulations! You guessed the right number.")
 else:
     print(f"Sorry! The number was {number}.")
